bcf9caa79d06dad235d23f16daf4192d08f9671b
Yury Korolev <yury.korolev@gmail.com>
2021-10-21 19:47:34 +0300

    Initial Commit

---
+++ TestWSS/TestWSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestWSSApp.swift
+//  TestWSS
+//
+//  Created by Yury Korolev on 21.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestWSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
